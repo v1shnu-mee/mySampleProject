2e93c04c01fce59b4f77fd1f3e90b1a14aaea531
v1shnu-mee <vishnuvarathan.p@e2infosystems.com>
2016-02-03 11:26:07 +0530

    added location finding logic

--- mySampleProject/ViewController.swift
+++ mySampleProject/ViewController.swift
@@ -7,21 +7,52 @@
 //
 
 import UIKit
+import CoreLocation
+import MapKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController,CLLocationManagerDelegate {
+let locationManager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         print("hello")
         print("this is written only for branch 2")
+        
+        let locManager = CLLocationManager()
+        locManager.requestWhenInUseAuthorization()
+        
+        locationManager.requestWhenInUseAuthorization();
+        if CLLocationManager.locationServicesEnabled(){
+            locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+            locationManager.startUpdatingLocation()
+            
+        }
+        else{
+            print("Location service disabled");
+        }
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]){
+        
+        let locValue : CLLocationCoordinate2D = manager.location!.coordinate;
+        let long = locValue.longitude;
+        let lat = locValue.latitude;
+        
+        let coordinate1:CLLocationCoordinate2D = CLLocationCoordinate2D(latitude:11.9547, longitude:79.5277) //bes
+        let coordinate2:CLLocationCoordinate2D = CLLocationCoordinate2D(latitude:lat , longitude:long) // tir
+        
+        print("coo \(coordinate2)")
+        let point1:MKMapPoint = MKMapPointForCoordinate(coordinate1);
+        let point2:MKMapPoint = MKMapPointForCoordinate(coordinate2);
+        let distance:CLLocationDistance = MKMetersBetweenMapPoints(point1, point2)
+        let kilometer:CLLocationDistance = distance / 1000;
+        print("distance \(String(format:"%.1f", kilometer))")
+        locationManager.stopUpdatingLocation();
+        
+        //Do What ever you want with it
     }
-
 
 }
 
